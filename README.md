ca142b963b9683ab5c9d8b4221c5ae27560050c9
Graham Jenson <grahamjenson@maori.geek.nz>
2015-06-09 13:59:05 +0300

    added pmux and travis configs

---
+++ README.md
@@ -0,0 +1,23 @@
+To run the tests first install modules:
+
+```
+npm install
+```
+
+then install pmux
+
+```
+npm install -g pmux
+```
+
+then start the pmux configuration:
+
+```
+pmux microservice_configuration.js 
+```
+
+then execute the tests
+
+```
+mocha
+```
---
+++ microservice_configuration.js
@@ -0,0 +1,35 @@
+var microservices_directory = "services_dir"
+var serviceArepo = "git@github.com:grahamjenson/microservice_A.git"
+var serviceBrepo = "git@github.com:grahamjenson/microservice_B.git"
+
+var serviceAversion = process.env.SERVICE_A_VERSION
+var serviceBversion = process.env.SERVICE_B_VERSION
+
+var configuration = {
+  "name": "microservices",
+  "pre_commands": [
+    "rm -rf " + microservices_directory,
+    "mkdir " + microservices_directory
+  ],
+  "windows": {
+    "serviceA": {
+      "commands": [
+        "git clone " + serviceArepo + " -b " + serviceAversion,
+        "cd microservice_A",
+        "node service.js"
+      ],
+      "dir" : microservices_directory
+    },
+    "serviceB": {
+      "commands": [
+        "git clone " + serviceBrepo + " -b " + serviceBversion,
+        "cd microservice_B",
+        "node service.js"
+      ],
+      "dir" : microservices_directory
+    }
+  }
+}
+
+
+module.exports = configuration
